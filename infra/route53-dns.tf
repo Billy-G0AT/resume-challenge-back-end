cbf04d870d542e0b80cc14d8b72177008396bd68
Billy Euceda <billymm314@gmail.com>
2023-08-22 12:15:05 -0700

    added subdomain record

--- infra/route53-dns.tf
+++ infra/route53-dns.tf
@@ -13,3 +13,15 @@ resource "aws_route53_record" "domain" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "subdomain" {
+  name    = var.subdomain
+  type    = "A"
+  zone_id = aws_route53_zone.this.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.this.domain_name
+    zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
