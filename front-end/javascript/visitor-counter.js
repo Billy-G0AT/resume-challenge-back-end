d68ee16b109ac302ceab4789486da0324b2d5aa8
Billy Euceda <billymm314@gmail.com>
2023-09-22 12:54:40 -0700

    folder re-structure

---
+++ front-end/javascript/visitor-counter.js
@@ -0,0 +1,19 @@
+fetch('https://080sgfhfqe.execute-api.us-west-1.amazonaws.com/dev/visit-count-function')
+  .then(response => {
+    // Checks if the request was successful (status 200)
+    if (response.ok) {
+      // Extracts the response as text
+      return response.text();
+    } 
+    else {
+      throw new Error('Request failed with status ' + response.status);
+    }
+  })
+  .then(data => {
+    const counter = document.getElementById('counter');
+    counter.textContent = data;
+  })
+  .catch(error => {
+    // Handles any errors that occurred during the request
+    console.error('Error:', error);
+  });
