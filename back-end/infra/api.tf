1d27f3a58df37050b1f7d4f97703c0b4fbb41e1c
Billy Euceda <billymm314@gmail.com>
2024-03-22 19:35:35 -0700

    stage6

--- back-end/infra/api.tf
+++ back-end/infra/api.tf
@@ -23,7 +23,7 @@ resource "aws_apigatewayv2_api" "this" {
 #* API Stage
 resource "aws_apigatewayv2_stage" "this" {
   api_id      = aws_apigatewayv2_api.this.id
-  name        = "dev5"
+  name        = "dev6"
   auto_deploy = true
 }
 
